19db8d8218b0e8475f97436c3d20d6bd6d070f2a
Luiz <luiz.p.trein@gmail.com>
2026-07-23 22:08:21 -0300

    feat: add buttons component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/assets/images/banner.jpg"
+import { Button } from "./Button/Button"
 
 export const Hero = () => {
   return (
@@ -6,14 +7,14 @@ export const Hero = () => {
       <section className="relative h-125 rounded-[20px] mb-10">
         <img src={Banner} alt="Banner homem sentado com os tenis da SyntaxWear" className="w-full h-full object-cover rounded-[20px]" />
 
-        <div className="absolute w-full bottom-0 flex justify-end items-center text-center px-9 md:px24 pb-32">
+        <div className="absolute w-full bottom-0 flex justify-end items-center text-center px-6 md:px-24 pb-32">
           <div className="text-white flex flex-col items-center w-97">
             <h2 className="text-xl font-medium leading-normal tracking-wider mb-2.5">Krypton One</h2>
             <h1 className="text-2xl leading-9 tracking-widest mb-10">Transforme qualquer passo em presenca</h1>
 
             <div className="flex gap-3.5">
-              <button>Ver modelos</button>
-              <button>Comprar</button>
+              <Button variant="secondary" size="sm">Ver modelos</Button>
+              <Button variant="primary">Comprar</Button>
             </div>
           </div>
         </div>
