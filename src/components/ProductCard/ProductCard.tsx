4603218404879fc89491bd796d9c6babdf889ccf
Luiz <luiz.p.trein@gmail.com>
2026-08-01 23:49:45 -0300

    feat: add products list mockup to app

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,30 @@
+import { Link } from "@tanstack/react-router"
+import { MdAddShoppingCart } from "react-icons/md"
+import type { Product } from "../../interfaces/product"
+
+interface ProductCardPorps{
+  product: Product
+}
+
+export const ProductCard = ({product}: ProductCardPorps) => {
+  return (
+    <div className="rounded-2xl bg-white shadow-md">
+      <Link to="/products">
+        <img className="w-full max-h-100 object-cover rounded-md mb-2" src={product.image} alt={product.name} />
+      </Link>
+
+      <div className="text-black rounded-2xl p-4">
+        <h3 className="text-lg font-semibold">{product.name}</h3>
+        <p>{product.color}</p>
+
+        <div className="flex justify-between mt-2.5">
+          <p className="font-bold">R${product.price},00</p>
+
+          <button className="cursor-pointer">
+            <MdAddShoppingCart className="2-7 h-7"></MdAddShoppingCart>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/_app/products/index.tsx
+++ src/pages/_app/products/index.tsx
@@ -1,13 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { ProductList } from '../../../components/ProductList/ProductList'
+import { products } from '../../../mocks/products'
 
 export const Route = createFileRoute('/_app/products/')({
-  component: RouteComponent,
+  component: RouteComponent
 })
 
 function RouteComponent() {
   return (
-    <div className='container'>
-
+    <div className='container pt-44 md:pt-54 pb-10 md:px-10 px-10 mb-10 text-black bg-surface'>
+      <h1 className='text-center mb-3 text-3xl'>Lista de Produtos</h1>
+      <h2 className='text-center p-2 mb-10'>Conforto excepcional para suas aventuras do dia a dia </h2>
+      <ProductList products={products}></ProductList>
     </div>
   )
 }
