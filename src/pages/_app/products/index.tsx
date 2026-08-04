41fee76623ddcb8df3d80f8d8d37377f56e075a9
Luiz <luiz.p.trein@gmail.com>
2026-08-03 22:08:48 -0300

    chore: refactoring of styles and missing atributes

--- src/pages/_app/products/index.tsx
+++ src/pages/_app/products/index.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/_app/products/')({
 
 function RouteComponent() {
   return (
-    <div className='container pt-44 md:pt-54 pb-10 md:px-10 px-10 mb-10 text-black bg-surface'>
+    <div className='container pt-44 md:pt-54 pb-10 md:px-10 px-10 mb-10 text-black'>
       <h1 className='text-center mb-3 text-3xl'>Lista de Produtos</h1>
       <h2 className='text-center p-2 mb-10'>Conforto excepcional para suas aventuras do dia a dia </h2>
       <ProductList products={products}></ProductList>
